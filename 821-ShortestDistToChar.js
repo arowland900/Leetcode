da8e7f4c357f33a610befcf305264f12e08054bc
Alexander Rowland <arowland900@gmail.com>
2021-07-13 17:58:12 -0700

    add 821-ShortestDistToChar solution 2

--- 821-ShortestDistToChar.js
+++ 821-ShortestDistToChar.js
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/shortest-distance-to-a-character/
 
 // first solution
 var shortestToChar = function (s, c) {
@@ -19,5 +20,27 @@ var shortestToChar = function (s, c) {
             answer.push(min)
         }
     }
+    return answer
+};
+
+// second solution (no nested loops)
+var shortestToChar = function (s, c) {
+
+    let allCs = []
+    let answer = []
+    let n = s.length
+
+    let prev = -Infinity
+    for (let i = 0; i < n; i++) {
+        if (s[i] == c) prev = i
+        answer[i] = (i - prev)
+    }
+
+    prev = Infinity
+    for (let i = n - 1; i >= 0; i--) {
+        if (s[i] == c) prev = i
+        answer[i] = Math.min(answer[i], prev - i)
+    }
+
     return answer
 };
