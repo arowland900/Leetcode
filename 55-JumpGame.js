bcfc0300a26486c0ae0049403b397c7616549f7a
Alexander Rowland <arowland900@gmail.com>
2021-07-20 22:09:50 -0700

    add 55-JumpGame solution 2

--- 55-JumpGame.js
+++ 55-JumpGame.js
@@ -10,3 +10,17 @@ var canJump = function (nums) {
 
     return !n
 };
+
+// Solution 2 
+var canJump = function (nums) {
+    if (nums.length == 1) return true
+
+    let max = nums[0]
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        max = Math.max(max, i + nums[i])
+        if (max >= nums.length - 1) return true
+        if (nums[i] == 0 && max <= i) return false
+    }
+    return false
+};
