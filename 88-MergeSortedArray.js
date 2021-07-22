b993b0a28f42163b0761c754c43bb9f2d742f13f
Alexander Rowland <arowland900@gmail.com>
2021-07-22 14:57:00 -0700

    add 88-MergeSortedArray solution 2

--- 88-MergeSortedArray.js
+++ 88-MergeSortedArray.js
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/merge-sorted-array/
 
+// solution 1: TC: O(m) SC: O(1)
 var merge = function (nums1, m, nums2, n) {
     let x = nums1.length
     nums1.splice(m, x - m)
@@ -11,5 +12,14 @@ var merge = function (nums1, m, nums2, n) {
             nums1.splice(i, 0, nums2[j])
             j++
         }
+    }
+};
+
+// Solution 2: TC: O(m), SC: O(1)
+var merge = function (nums1, m, nums2, n) {
+    let i = m - 1, j = n - 1, k = m + n - 1;
+
+    while (j >= 0) {
+        nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--]
     }
 };
