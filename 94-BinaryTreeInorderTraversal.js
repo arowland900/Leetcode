6432f22df15499f6ec3a357e79f582ec0fd44bea
Alexander Rowland <arowland900@gmail.com>
2021-07-26 17:45:21 -0700

    add 94-BinaryTreeInorderTraversal recursive solution

--- 94-BinaryTreeInorderTraversal.js
+++ 94-BinaryTreeInorderTraversal.js
@@ -1,3 +1,15 @@
 
 
 // Recursive
+function helper(node, res) {
+    if (node) {
+        if (node.left) helper(node.left, res)
+        res.push(node.val)
+        if (node.right) helper(node.right, res)
+    }
+}
+var inorderTraversal = function (root) {
+    let x = []
+    helper(root, x)
+    return x
+};
