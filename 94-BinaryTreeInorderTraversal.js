91481894d1c525c1b039a0307f6ce740f0471577
Alexander Rowland <arowland900@gmail.com>
2021-07-26 18:19:14 -0700

    add 94-BinaryTreeInorderTraversal iterative solution

--- 94-BinaryTreeInorderTraversal.js
+++ 94-BinaryTreeInorderTraversal.js
@@ -13,3 +13,20 @@ var inorderTraversal = function (root) {
     helper(root, x)
     return x
 };
+
+// Iterative
+var inorderTraversal = function (root) {
+    let res = []
+    let stack = []
+    let current = root
+    while (current || stack.length) {
+        while (current) {
+            stack.push(current)
+            current = current.left
+        }
+        current = stack.pop()
+        res.push(current.val)
+        current = current.right
+    }
+    return res
+};
