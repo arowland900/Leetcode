be54cd6a53351af6c7d72c6f7b3137727e5635e8
Alexander Rowland <arowland900@gmail.com>
2021-07-29 12:46:27 -0700

    add 590-NaryTreePostorderTraversal recursive 2 solution

--- 590-NaryTreePostorderTraversal.js
+++ 590-NaryTreePostorderTraversal.js
@@ -1,14 +1,29 @@
 // https://leetcode.com/problems/n-ary-tree-postorder-traversal/submissions/
 
 // Recursive 1
-var postorder = function(root, stack=[root], res=[]) {
-    if(!root || stack.length == 0) return res
-    if(stack.length){
+var postorder = function (root, stack = [root], res = []) {
+    if (!root || stack.length == 0) return res
+    if (stack.length) {
         let node = stack.pop()
-        res.unshift(node.val)  
-        for(c of node.children) stack.push(c)
+        res.unshift(node.val)
+        for (c of node.children) stack.push(c)
         return postorder(node, stack, res)
     } else return res
+};
+
+// Recursive 2
+var postorder = function (root) {
+    const res = [];
+    traverse(root);
+    return res;
+
+    function traverse(node) {
+        if (!node) return;
+        for (child of node.children) {
+            traverse(child);
+        }
+        res.push(node.val);
+    }
 };
 
 // Iterative
