f63c465739ca0aabd4cf61dad65e6e8c08cf0c54
Alexander Rowland <arowland900@gmail.com>
2021-07-30 16:07:26 -0700

    add 429-NaryTreeLevelOrderTraversal iterative solution

--- 429-NaryTreeLevelOrderTraversal.js
+++ 429-NaryTreeLevelOrderTraversal.js
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/n-ary-tree-level-order-traversal/
 
+// Recursive
 var levelOrder = function (root, level = 0, res = []) {
     if (!root) return []
 
@@ -9,5 +10,26 @@ var levelOrder = function (root, level = 0, res = []) {
     for (c of root.children) {
         levelOrder(c, level + 1, res)
     }
+    return res
+};
+
+// Iterative
+var levelOrder = function (root) {
+    if (!root) return []
+
+    let res = []
+    let queue = [root]
+
+    while (queue.length) {
+        let level = []
+        let q = queue.length
+        for (let i = 0; i < q; i++) {
+            let node = queue.shift()
+            level.push(node.val)
+            queue.push(...node.children)
+        }
+        res.push(level)
+    }
+
     return res
 };
