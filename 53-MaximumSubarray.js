4f1c664fa30534da88a7f28a57481980d52dff35
Alexander Rowland <arowland900@gmail.com>
2021-08-12 22:43:59 -0700

    add 53-MaximumSubarray solution

---
+++ 53-MaximumSubarray.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/maximum-subarray/
+
+var maxSubArray = function (nums) {
+
+    let max = nums[0], currMax = nums[0]
+
+    for (let i = 1; i < nums.length; i++) {
+        currMax = Math.max(currMax += nums[i], nums[i])
+        max = Math.max(currMax, max)
+    }
+
+    return max
+};
