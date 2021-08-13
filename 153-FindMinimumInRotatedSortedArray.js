c71c1fc2d3bf71e1828eb94d2cf40f3e3d2993fd
Alexander Rowland <arowland900@gmail.com>
2021-08-13 15:48:40 -0700

    add 153-FindMinimumInRotatedSortedArray iterative solution

--- 153-FindMinimumInRotatedSortedArray.js
+++ 153-FindMinimumInRotatedSortedArray.js
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
+// Recursive bin search
 var findMin = function (arr) {
 
     if (arr.length == 1) return arr[0]
@@ -10,4 +11,25 @@ var findMin = function (arr) {
 
     return Math.min(findMin(left), findMin(right))
 
+};
+
+// Iterative bin search
+var findMin = function (nums) {
+
+    if (nums.length == 1) return nums[0]
+
+    let lo = 0, hi = nums.length - 1
+
+    while (lo < hi) {
+        let mid = Math.floor(lo + (hi - lo) / 2)
+        if (nums[lo] < nums[hi]) {
+            return nums[lo]
+        }
+        if (nums[mid] >= nums[lo]) {
+            lo = mid + 1
+        } else {
+            hi = mid
+        }
+    }
+    return nums[lo]
 };
