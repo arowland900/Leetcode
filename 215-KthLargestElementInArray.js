9339f949c9b0f6be8d89e3bcf38e4ceb9fa1f4b6
Alexander Rowland <arowland900@gmail.com>
2021-08-17 11:07:29 -0700

    add 215-KthLargestElement solution 1

--- 215-KthLargestElementInArray.js
+++ 215-KthLargestElementInArray.js
@@ -1,1 +1,6 @@
 // https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+// Solution 1 
+var findKthLargest = function (nums, k) {
+    return nums.sort((a, b) => Number(b) - Number(a))[k - 1]
+};
