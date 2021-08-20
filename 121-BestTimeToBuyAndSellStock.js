3d10b462417fcde22aa9753a560ce28b2bb0d479
Alexander Rowland <arowland900@gmail.com>
2021-08-20 16:46:37 -0700

    add 121-BestTimeToBuyAndSellStock alternate solution

--- 121-BestTimeToBuyAndSellStock.js
+++ 121-BestTimeToBuyAndSellStock.js
@@ -10,3 +10,14 @@ var maxProfit = function (prices) {
     }
     return profit
 };
+
+var maxProfit = function(prices) {
+    let max = 0
+    let tempMax = 0
+    for(let i = 1; i < prices.length; i++){
+        tempMax += prices[i] - prices[i-1]
+        tempMax = Math.max(tempMax, 0)
+        max = Math.max(max, tempMax)
+    }
+    return max
+};
