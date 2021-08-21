48d34dffae922acce5774b495248b9968a63b0c2
Alexander Rowland <arowland900@gmail.com>
2021-08-20 19:36:47 -0700

    add 300 tabulation solution

--- 300-LongestIncreasingSubsequence.js
+++ 300-LongestIncreasingSubsequence.js
@@ -1,1 +1,16 @@
 // https://leetcode.com/problems/longest-increasing-subsequence/
+
+// Tabulation Solution
+var lengthOfLIS = function(nums) {
+    let table = new Array(nums.length).fill(1)
+    
+    for(let i = 1; i < table.length; i++){
+        for(let j = 0; j < i; j++){
+            if(nums[j] < nums[i]){
+                table[i] = Math.max(table[i], table[j]+1)
+            } 
+        } 
+    }
+    
+    return Math.max(...table)
+};
