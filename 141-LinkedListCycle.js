0bc718acb553935633de0582132b686eb4004998
Alexander Rowland <arowland900@gmail.com>
2021-08-22 23:01:34 -0700

    add 141 solution

--- 141-LinkedListCycle.js
+++ 141-LinkedListCycle.js
@@ -1,1 +1,11 @@
 // https://leetcode.com/problems/linked-list-cycle/
+
+var hasCycle = function(head) {
+    
+    while(head){
+        if(head.checked) return true
+        head.checked = true
+        head = head.next
+    }
+    return false
+};
